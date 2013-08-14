e926adada2476bf2c61534a29b6fda79fa2e8c2f
Wes Hardaker <opensource@hardakers.net>
2013-08-14 15:02:57 -0700

    add timing tests for ldns (doesn't seem to do dnssec)

--- ldns_test.c
+++ ldns_test.c
@@ -1,5 +1,5 @@
 /*
- * mx is a small program that prints out the mx records
+ * a is a small program that prints out the a records
  * for a particular domain
  * (c) NLnet Labs, 2005 - 2008
  * See the file LICENSE for the license
@@ -18,7 +18,7 @@ main(int argc, char *argv[])
         ldns_resolver *res;
         ldns_rdf *domain;
         ldns_pkt *p;
-        ldns_rr_list *mx;
+        ldns_rr_list *a;
         ldns_status s;
         
         struct timeval starttime, endtime;
@@ -31,7 +31,7 @@ main(int argc, char *argv[])
         }
 
         p = NULL;
-        mx = NULL;
+        a = NULL;
         domain = NULL;
         res = NULL;
         
@@ -45,7 +45,7 @@ main(int argc, char *argv[])
                 exit(EXIT_FAILURE);
         }
 
-        /* use the resolver to send a query for the mx 
+        /* use the resolver to send a query for the a 
          * records of the domain given on the command line
          */
         p = ldns_resolver_query(res,
@@ -59,27 +59,40 @@ main(int argc, char *argv[])
         if (!p)  {
                 exit(EXIT_FAILURE);
         } else {
-                /* retrieve the MX records from the answer section of that
+                /* retrieve the A records from the answer section of that
                  * packet
                  */
-                mx = ldns_pkt_rr_list_by_type(p,
+                a = ldns_pkt_rr_list_by_type(p,
                                               LDNS_RR_TYPE_A,
                                               LDNS_SECTION_ANSWER);
-                if (!mx) {
+                if (!a) {
                         fprintf(stderr, 
-                                        " *** invalid answer name %s after MX query for %s\n",
+                                        " *** invalid answer name %s after A query for %s\n",
                                         argv[1], argv[1]);
                         ldns_pkt_free(p);
                         ldns_resolver_deep_free(res);
                         exit(EXIT_FAILURE);
                 } else {
-                        ldns_rr_list_sort(mx); 
-                        ldns_rr_list_print(stdout, mx);
-                        ldns_rr_list_deep_free(mx);
+                        ldns_rr_list_sort(a); 
+                        ldns_rr_list_print(stdout, a);
+                        ldns_rr_list_deep_free(a);
                 }
         }
         ldns_pkt_free(p);
 
+        fprintf(stderr, "starting %d queries....\n", number);
+
+        // actually run the tests, timing them
+        gettimeofday(&starttime, NULL);
+        for (i = 0; i < number; i++) {
+            p = ldns_resolver_query(res,
+                                    domain,
+                                    LDNS_RR_TYPE_A,
+                                    LDNS_RR_CLASS_IN,
+                                    LDNS_RD);
+        }
+        gettimeofday(&endtime, NULL);
+        fprintf(stderr, "time elapsed (ms) for %d queries: %d\n", number, timeofday_diff(&starttime, &endtime));
 
 
         
